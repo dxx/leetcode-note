8ed724dbb8d77784eaa7908d31bbf5020aef06ef
code-mcx <1827939333@qq.com>
2020-02-16 21:38:04 +0800

    两数之和 :memo:

--- code-java/src/com/mcx/twosum/Main.java
+++ code-java/src/com/mcx/twosum/Main.java
@@ -1,12 +1,10 @@
-package com.mcx;
+package com.mcx.twosum;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by mcx on 2020-06-28.
- *
  * 两数之和
  * 给定一个整数数组 `nums` 和一个目标值 `target`，请你在该数组中找出和为目标值的那**两个**整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -16,7 +14,7 @@ import java.util.Map;
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
  */
-public class TwoSum {
+public class Main {
 
     public static int[] towSum(int[] nums, int target) {
         int[] indexes = new int[2];
