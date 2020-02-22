6361c4c3880570314cf14d5c787a4fc6cda630dc
code-mcx <1827939333@qq.com>
2020-02-22 21:26:46 +0800

    回文数 :memo:

--- code-java/src/com/mcx/palindromenum/Main.java
+++ code-java/src/com/mcx/palindromenum/Main.java
@@ -1,8 +1,6 @@
-package com.mcx;
+package com.mcx.palindromenum;
 
 /**
- * Created by mcx on 2020-06-29.
- *
  * 回文数
  * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
  *
@@ -18,7 +16,7 @@ package com.mcx;
  * 输出: false
  * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
  */
-public class PalindromeNum {
+public class Main {
 
     public static boolean isPalindrome(int num) {
         if (num < 0) {
