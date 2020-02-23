2038eee8324bcf0949265991fa99a8b111a2871d
code-mcx <1827939333@qq.com>
2020-02-23 21:25:07 +0800

    三数之和 :memo:

--- code-java/src/com/mcx/threesum/Main.java
+++ code-java/src/com/mcx/threesum/Main.java
@@ -1,12 +1,10 @@
-package com.mcx;
+package com.mcx.threesum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created by mcx on 2020-09-26.
- *
  * 三数之和
  * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
  * 注意: 答案中不可以包含重复的三元组。
@@ -19,7 +17,7 @@ import java.util.List;
  *   [-1, -1, 2]
  * ]
  */
-public class ThreeSum {
+public class Main {
 
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
