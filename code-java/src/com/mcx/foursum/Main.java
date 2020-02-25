c9b2b95dcd1bd2003fbdcb0b0f6c441b81a90512
code-mcx <1827939333@qq.com>
2020-02-25 21:21:46 +0800

    四数之和 :memo:

--- code-java/src/com/mcx/foursum/Main.java
+++ code-java/src/com/mcx/foursum/Main.java
@@ -1,12 +1,10 @@
-package com.mcx;
+package com.mcx.foursum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created by mcx on 2020-10-05.
- *
  * 四数之和
  * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d 
  * 使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
@@ -21,8 +19,8 @@ import java.util.List;
  *   [-2,  0, 0, 2]
  * ]
  */
-public class FourSum {
-
+public class Main {
+    
     public static List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
 
