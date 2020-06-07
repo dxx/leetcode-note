6a662761eff15e5feeea7a74c0f13c59cdcc8e1a
code-mcx <1827939333@qq.com>
2020-06-07 14:23:01 +0800

    相同的树 :memo:

--- code-go/sametree/main.go
+++ code-go/sametree/main.go
@@ -1,4 +1,4 @@
-package main
+package sametree
 
 import "fmt"
 
@@ -62,73 +62,4 @@ func infixOrder(node *TreeNode)  {
     infixOrder(node.Left)
     fmt.Print(node.Val)
     infixOrder(node.Right)
-}
-
-func main() {
-    p := &TreeNode{Val: 1}
-    p2 := &TreeNode{Val: 2}
-    p3 := &TreeNode{Val: 3}
-    p.Left = p2
-    p.Right = p3
-
-    q := &TreeNode{Val: 1}
-    q2 := &TreeNode{Val: 2}
-    q3 := &TreeNode{Val: 3}
-    q.Left = q2
-    q.Right = q3
-
-    fmt.Print("p: ")
-    infixOrder(p)
-    fmt.Println()
-
-    fmt.Print("q: ")
-    infixOrder(q)
-    fmt.Println()
-
-    isSame := isSameTree(p, q)
-    fmt.Println(isSame)
-
-
-    p = &TreeNode{Val: 1}
-    p2 = &TreeNode{Val: 2}
-    p.Left = p2
-
-    q = &TreeNode{Val: 1}
-    q2 = &TreeNode{Val: 2}
-    q.Right = q2
-
-    fmt.Print("p: ")
-    infixOrder(p)
-    fmt.Println()
-
-    fmt.Print("q: ")
-    infixOrder(q)
-    fmt.Println()
-
-    isSame = isSameTree(p, q)
-    fmt.Println(isSame)
-
-
-    p = &TreeNode{Val: 1}
-    p2 = &TreeNode{Val: 2}
-    p3 = &TreeNode{Val: 1}
-    p.Left = p2
-    p.Right = p3
-
-    q = &TreeNode{Val: 1}
-    q2 = &TreeNode{Val: 1}
-    q3 = &TreeNode{Val: 2}
-    q.Left = q2
-    q.Right = q3
-
-    fmt.Print("p: ")
-    infixOrder(p)
-    fmt.Println()
-
-    fmt.Print("q: ")
-    infixOrder(q)
-    fmt.Println()
-
-    isSame = isSameTree(p, q)
-    fmt.Println(isSame)
 }
