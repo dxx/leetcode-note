88870048e7a88c20062230c9a2ecb23376eef874
code-mcx <1827939333@qq.com>
2020-06-09 14:16:38 +0800

    二叉树的最大深度 :memo:

--- code-go/maximumdepthofbinarytree/main.go
+++ code-go/maximumdepthofbinarytree/main.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package maximumdepthofbinarytree
 
 // 二叉树的最大深度
 // 给定一个二叉树，找出其最大深度。二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
@@ -38,19 +36,4 @@ func max(a, b int) int {
         return a
     }
     return b
-}
-
-func main() {
-    root := &TreeNode{Val: 3}
-    node1 := &TreeNode{Val: 9}
-    node2 := &TreeNode{Val: 20}
-    node3 := &TreeNode{Val: 15}
-    node4 := &TreeNode{Val: 7}
-    root.Left = node1
-    root.Right = node2
-    node2.Left = node3
-    node2.Right = node4
-
-    depth := maxDepth(root)
-    fmt.Println(depth)
 }
