9ef55bd528d6118d16f3b566ccb9e8fd64c19aeb
code-mcx <1827939333@qq.com>
2020-06-10 14:14:24 +0800

    二叉树的层序遍历 :memo:

--- code-go/binarytreelevelordertraversal/levelorder/main.go
+++ code-go/binarytreelevelordertraversal/levelorder/main.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package levelorder
 
 // 二叉树的层序遍历
 // 给你一个二叉树，请你返回其按 **层序遍历** 得到的节点值。 （即逐层地，从左到右访问所有节点）。
@@ -89,19 +87,4 @@ func dfs(node *TreeNode, level int, results [][]int) [][]int {
         results = rightResults
     }
     return results
-}
-
-func main() {
-    root := &TreeNode{Val: 3}
-    node1 := &TreeNode{Val: 9}
-    node2 := &TreeNode{Val: 20}
-    node3 := &TreeNode{Val: 15}
-    node4 := &TreeNode{Val: 7}
-    root.Left = node1
-    root.Right = node2
-    node2.Left = node3
-    node2.Right = node4
-
-    results := levelOrder(root)
-    fmt.Println(results)
 }
