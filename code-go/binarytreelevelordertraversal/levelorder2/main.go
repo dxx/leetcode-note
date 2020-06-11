90951559673354d1fcbef7f43998d371bddc246d
code-mcx <1827939333@qq.com>
2020-06-11 14:09:10 +0800

    二叉树的层序遍历 2 :memo:

--- code-go/binarytreelevelordertraversal/levelorder2/main.go
+++ code-go/binarytreelevelordertraversal/levelorder2/main.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package levelorder2
 
 // 二叉树的层次遍历 II
 // 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
@@ -58,19 +56,4 @@ func levelOrderBottom(root *TreeNode) [][]int {
         q = p
     }
     return results
-}
-
-func main() {
-    root := &TreeNode{Val: 3}
-    node1 := &TreeNode{Val: 9}
-    node2 := &TreeNode{Val: 20}
-    node3 := &TreeNode{Val: 15}
-    node4 := &TreeNode{Val: 7}
-    root.Left = node1
-    root.Right = node2
-    node2.Left = node3
-    node2.Right = node4
-
-    results := levelOrderBottom(root)
-    fmt.Println(results)
 }
---
+++ code-go/binarytreelevelordertraversal/levelorder2/main_test.go
@@ -0,0 +1,18 @@
+package levelorder2
+
+import "testing"
+
+func TestLevelOrderBottom(t *testing.T) {
+	root := &TreeNode{Val: 3}
+	node1 := &TreeNode{Val: 9}
+	node2 := &TreeNode{Val: 20}
+	node3 := &TreeNode{Val: 15}
+	node4 := &TreeNode{Val: 7}
+	root.Left = node1
+	root.Right = node2
+	node2.Left = node3
+	node2.Right = node4
+
+	results := levelOrderBottom(root)
+	t.Log(results)
+}
