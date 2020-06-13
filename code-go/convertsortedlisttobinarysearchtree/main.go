fae049e059e49bcc01b7030bce9f392a3e5e8dba
code-mcx <1827939333@qq.com>
2020-06-13 14:00:43 +0800

    有序链表转换二叉搜索树 :memo:

--- code-go/convertsortedlisttobinarysearchtree/main.go
+++ code-go/convertsortedlisttobinarysearchtree/main.go
@@ -1,4 +1,4 @@
-package main
+package convertsortedlisttobinarysearchtree
 
 import "fmt"
 
@@ -98,20 +98,4 @@ func infixOrder(node *TreeNode) {
     infixOrder(node.Left)
     fmt.Println(node.Val)
     infixOrder(node.Right)
-}
-
-func main() {
-    node1 := &ListNode{Val: -10}
-    node2 := &ListNode{Val: -3}
-    node3 := &ListNode{Val: 0}
-    node4 := &ListNode{Val: 5}
-    node5 := &ListNode{Val: 9}
-    node1.Next = node2
-    node2.Next = node3
-    node3.Next = node4
-    node4.Next = node5
-
-    tree := sortedListToBST(node1)
-
-    infixOrder(tree)
 }
