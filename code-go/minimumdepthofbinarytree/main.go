8cbf490ddc85051cb80450abfc74915d40ff3da3
code-mcx <1827939333@qq.com>
2020-06-15 13:51:41 +0800

    二叉树的最小深度 :memo:

--- code-go/minimumdepthofbinarytree/main.go
+++ code-go/minimumdepthofbinarytree/main.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package minimumdepthofbinarytree
 
 // 二叉树的最小深度
 // 给定一个二叉树，找出其最小深度。
@@ -47,19 +45,4 @@ func min(a, b int) int {
         return a
     }
     return b
-}
-
-func main() {
-    root := &TreeNode{Val: 3}
-    node1 := &TreeNode{Val: 9}
-    node2 := &TreeNode{Val: 20}
-    node3 := &TreeNode{Val: 15}
-    node4 := &TreeNode{Val: 7}
-    root.Left = node1
-    root.Right = node2
-    node2.Left = node3
-    node2.Right = node4
-
-    depth := minDepth(root)
-    fmt.Println(depth)
 }
---
+++ code-go/minimumdepthofbinarytree/main_test.go
@@ -0,0 +1,18 @@
+package minimumdepthofbinarytree
+
+import "testing"
+
+func TestMinDepth(t *testing.T) {
+	root := &TreeNode{Val: 3}
+	node1 := &TreeNode{Val: 9}
+	node2 := &TreeNode{Val: 20}
+	node3 := &TreeNode{Val: 15}
+	node4 := &TreeNode{Val: 7}
+	root.Left = node1
+	root.Right = node2
+	node2.Left = node3
+	node2.Right = node4
+
+	depth := minDepth(root)
+	t.Log(depth)
+}
