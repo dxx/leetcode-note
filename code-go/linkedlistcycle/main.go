cc72c9f28eace3f5e8930ed9094702d8f15cddee
code-mcx <1827939333@qq.com>
2020-06-17 13:45:26 +0800

    环形链表 :memo:

--- code-go/linkedlistcycle/main.go
+++ code-go/linkedlistcycle/main.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package linkedlistcycle
 
 // 环形链表
 // 给定一个链表，判断链表中是否有环。
@@ -40,31 +38,4 @@ func hasCycle(head *ListNode) bool {
         }
     }
     return false
-}
-
-func main() {
-    head := &ListNode{Val: 3}
-    node2 := &ListNode{Val: 2}
-    node3 := &ListNode{Val: 0}
-    node4 := &ListNode{Val: -4}
-    head.Next = node2
-    node2.Next = node3
-    node3.Next = node4
-    node4.Next = node2
-
-    result := hasCycle(head)
-    fmt.Println(result)
-
-    head = &ListNode{Val: 1}
-    node2 = &ListNode{Val: 2}
-    head.Next = node2
-    node2.Next = head
-
-    result = hasCycle(head)
-    fmt.Println(result)
-
-    head = &ListNode{Val: 1}
-
-    result = hasCycle(head)
-    fmt.Println(result)
 }
