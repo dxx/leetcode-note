5be71c2c7f252dd4d4543ac24bdeeec50166dacc
code-mcx <1827939333@qq.com>
2020-06-20 12:00:35 +0800

    对链表进行插入排序 :memo:

--- code-go/insertionsortlist/main.go
+++ code-go/insertionsortlist/main.go
@@ -1,4 +1,4 @@
-package main
+package insertionsortlist
 
 import (
     "fmt"
@@ -64,36 +64,4 @@ func printListNode(node *ListNode) {
     }
     str = strings.TrimSuffix(str, "->")
     fmt.Println(str)
-}
-
-func main() {
-    node1 := &ListNode{Val: 4}
-    node2 := &ListNode{Val: 2}
-    node3 := &ListNode{Val: 1}
-    node4 := &ListNode{Val: 3}
-    node1.Next = node2
-    node2.Next = node3
-    node3.Next = node4
-
-    printListNode(node1)
-
-    node := insertionSortList(node1)
-
-    printListNode(node)
-
-    node1 = &ListNode{Val: -1}
-    node2 = &ListNode{Val: 5}
-    node3 = &ListNode{Val: 3}
-    node4 = &ListNode{Val: 4}
-    node5 := &ListNode{Val: 0}
-    node1.Next = node2
-    node2.Next = node3
-    node3.Next = node4
-    node4.Next = node5
-
-    printListNode(node1)
-
-    node = insertionSortList(node1)
-
-    printListNode(node)
 }
