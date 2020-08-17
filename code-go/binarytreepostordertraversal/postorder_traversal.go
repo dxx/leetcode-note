71e83510b475a06371c6cd4c77e92b5b4e68637c
code-mcx <1827939333@qq.com>
2020-08-17 22:35:31 +0800

    二叉树的后序遍历 :memo:

---
+++ code-go/binarytreepostordertraversal/postorder_traversal.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+
+// 二叉树的后序遍历
+// 给定一个二叉树，返回它的 后序 遍历。
+
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+// 示例
+// 输入: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+// 输出: [3,2,1]
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+    // 非递归
+    return postorderNoRecursion(root)
+    // 递归
+    // return postorderRecursion(root)
+}
+
+// 非递归后序遍历
+func postorderNoRecursion(node *TreeNode) []int {
+    var stack []*TreeNode
+    output := make([]int, 0)
+    if node == nil {
+        return output
+    }
+    // 将当前节点压入栈
+    stack = append(stack, node)
+    for len(stack) > 0 {
+        // 弹出当前节点
+        node := stack[len(stack) - 1]
+        stack = stack[:len(stack) - 1]
+
+        out := make([]int, 1, len(output) + 1)
+        out[0] = node.Val
+        // 后访问的节点添加到最前面
+        output = append(out, output...)
+
+        if node.Left != nil {
+            // 先将左子节点压入栈, 先入栈的后出栈
+            stack = append(stack, node.Left)
+        }
+        if node.Right != nil {
+            // 将右子节点压入栈, 后入栈的先出栈
+            stack = append(stack, node.Right)
+        }
+
+    }
+    return output
+}
+
+// 递归后序遍历
+func postorderRecursion(node *TreeNode) []int {
+    if node == nil {
+        return []int{}
+    }
+    nodes := make([]int, 0)
+    // 先访问左子节点
+    nodes = append(nodes, postorderRecursion(node.Left)...)
+    // 再访问右子节点
+    nodes = append(nodes, postorderRecursion(node.Right)...)
+    // 最后将当前节点存入数组
+    nodes = append(nodes, node.Val)
+    return nodes
+}
+
+func main() {
+    node1 := &TreeNode{Val: 1}
+    node2 := &TreeNode{Val: 2}
+    node3 := &TreeNode{Val: 3}
+    node1.Right = node2
+    node2.Left = node3
+
+    results := postorderTraversal(node1)
+
+    fmt.Println(results)
+}
---
+++ code-java/src/com/mcx/BinaryTreePostorderTraversal.java
@@ -0,0 +1,94 @@
+package com.mcx;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by mcx on 2020-08-16.
+ *
+ * 二叉树的后序遍历
+ * 给定一个二叉树，返回它的 后序 遍历。
+ *
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ * 示例
+ * 输入: [1,null,2,3]
+ *   1
+ *    \
+ *     2
+ *    /
+ *   3
+ * 输出: [3,2,1]
+ */
+public class BinaryTreePostorderTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+    }
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        // 非递归
+        return postorderNoRecursion(root);
+        // 递归
+        /*List<Integer> output = new ArrayList<>();
+        postorderRecursion(root, output);
+        return output;*/
+    }
+
+    /**
+     * 非递归后序遍历
+     */
+    public static List<Integer> postorderNoRecursion(TreeNode node) {
+        Stack<TreeNode> stack = new Stack<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        // 将当前节点压入栈
+        stack.add(node);
+        while (!stack.isEmpty()) {
+            // 弹出当前节点
+            TreeNode treeNode = stack.pop();
+            // 后访问的节点添加到最前面
+            output.addFirst(treeNode.val);
+
+            if (treeNode.left != null) {
+                // 先将左子节点压入栈, 先入栈的后出栈
+                stack.push(treeNode.left);
+            }
+            if (treeNode.right != null) {
+                // 将右子节点压入栈, 后入栈的先出栈
+                stack.push(treeNode.right);
+            }
+        }
+        return output;
+    }
+
+    /**
+     * 递归后序遍历
+     */
+    public static void postorderRecursion(TreeNode node, List<Integer> output) {
+        if (node == null) {
+            return;
+        }
+        // 先访问左子节点
+        postorderRecursion(node.left, output);
+        // 再访问右子节点
+        postorderRecursion(node.right, output);
+        // 最后将当前节点存入数组
+        output.add(node.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        node1.right = node2;
+        node2.left = node3;
+
+        List<Integer> results = postorderTraversal(node1);
+
+        System.out.println(results);
+    }
+}
