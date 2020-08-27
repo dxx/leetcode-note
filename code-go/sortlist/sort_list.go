8c18264e0da7f1f9127abb6c48efaa2a53cfc360
code-mcx <1827939333@qq.com>
2020-08-27 14:17:50 +0800

    排序链表 :memo:

--- code-go/sortlist/sort_list.go
+++ code-go/sortlist/sort_list.go
@@ -1,1 +1,126 @@
-package sortlist
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+// 排序链表
+// 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+
+// 示例
+// 输入: 4->2->1->3
+// 输出: 1->2->3->4
+
+// 输入: -1->5->3->4->0
+// 输出: -1->0->3->4->5
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func sortList(head *ListNode) *ListNode {
+    return sort(head, nil)
+}
+
+// 分解排序
+func sort(head, tail *ListNode) *ListNode {
+    if head == tail {
+        return head
+    }
+    middleNode := getMiddleNode(head, tail)
+    // 保存下一个节点
+    next := middleNode.Next
+    // 断开链表
+    middleNode.Next = nil
+    // 分解左边
+    head1 := sort(head, middleNode)
+    // 分解右边
+    head2 := sort(next, tail)
+    // 将两个有序的链表合并成一个链表
+    return merge(head1, head2)
+}
+
+// 合并有序链表
+func merge(head1, head2 *ListNode) *ListNode {
+    current1, current2 := head1, head2
+
+    var appendNode *ListNode
+    newHead := &ListNode{Val: -1}
+    last := newHead
+    for current1 != nil && current2 != nil {
+        if current1.Val <= current2.Val {
+            appendNode = current1
+            current1 = current1.Next
+        } else {
+            appendNode = current2
+            current2 = current2.Next
+        }
+        last.Next = appendNode
+        last = appendNode
+    }
+
+    // 左边还有剩余节点
+    if current1 != nil {
+        last.Next = current1
+    }
+    // 右边还有剩余节点
+    if current2 != nil {
+        last.Next = current2
+    }
+
+    return newHead.Next
+}
+
+// 获取链表的中间节点
+func getMiddleNode(head, tail *ListNode) *ListNode {
+    fast, slow := head, head
+    for fast != tail && fast.Next != tail {
+        fast = fast.Next.Next
+        slow = slow.Next
+    }
+    return slow
+}
+
+func printListNode(node *ListNode) {
+    var str = ""
+    for node != nil {
+        str += fmt.Sprintf("%d->", node.Val)
+        node = node.Next
+    }
+    str = strings.TrimSuffix(str, "->")
+    fmt.Println(str)
+}
+
+func main() {
+    node1 := &ListNode{Val: 4}
+    node2 := &ListNode{Val: 2}
+    node3 := &ListNode{Val: 1}
+    node4 := &ListNode{Val: 3}
+    node1.Next = node2
+    node2.Next = node3
+    node3.Next = node4
+
+    printListNode(node1)
+
+    node := sortList(node1)
+
+    printListNode(node)
+
+    node1 = &ListNode{Val: -1}
+    node2 = &ListNode{Val: 5}
+    node3 = &ListNode{Val: 3}
+    node4 = &ListNode{Val: 4}
+    node5 := &ListNode{Val: 0}
+    node1.Next = node2
+    node2.Next = node3
+    node3.Next = node4
+    node4.Next = node5
+
+    printListNode(node1)
+
+    node = sortList(node1)
+
+    printListNode(node)
+}
